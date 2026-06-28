e84d4c285ae9c8de94d426b438cc563becb06c9d
MedAmine-Bj <mohamedamine.bejaoui@insat.ucar.tn>
2026-06-28 18:11:09 +0100

    Ajout de la fonction de calcul du cube

--- calcul.c
+++ calcul.c
@@ -3,6 +3,9 @@
 int main() {
     int nombre = 5;
     int carre = nombre * nombre;
+    int cube = nombre * nombre * nombre; // Nouvelle ligne !
+    
     printf("Le carre de %d est %d\n", nombre, carre);
+    printf("Le cube de %d est %d\n", nombre, cube); // Nouvelle ligne !
     return 0;
 }
